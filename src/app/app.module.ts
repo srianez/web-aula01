950fca33d410edc2633b6c094a46dfda082c9cec
logonrm <logonrm@fiap.com.br>
2017-06-14 22:20:12 -0300

    Importando ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+
+import { ReactiveFormsModule }
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule
